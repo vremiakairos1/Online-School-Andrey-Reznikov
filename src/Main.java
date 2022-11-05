38169c6073001ceebae4f0fd230c1695aa88e450
vremiakairos1 <116841743+vremiakairos1@users.noreply.github.com>
2022-11-06 00:27:59 +0300

    Update Main.java
    The code from the main branch was added to the GitHub - 4 objects. 
    And not the code from the feature branch - 5 objects.
    Editing was on github in order to merge the feature branch and the main branch on GitHub.

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,6 @@ public class Main {
         Lecture secondLecture = new Lecture();
         Lecture thirdLecture = new Lecture();
 
-        //Homework 5 main
-        Lecture sixthLecture = new Lecture();
-
         System.out.println(Lecture.counterLectures);
     }
 }
