65b8cdc208c20eab2e34399a02e4a45ba084200d
vremiakairos1 <odingradys@gmail.com>
2022-12-15 01:16:34 +0300

    "8. Added methods  When we pass for the first time - 9 lectures are created and a message about the limit is displayed  When we go through the second time, a few more lectures, we still create, the message about the limit is not displayed  At the end, the ability to return to the category and go through the steps again has been improved. However, the cycle of the last dialogue about the transition to the category should be finalized"

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ import models.Lecture;
 import java.util.Scanner;
 
 public class Main {
+    public static Course firstCourse;//для доступа к ID курса из другого класса
+                                     // или не из области видимости метода main
     public static void main(String[] args)
+
     {
-        Course firstCourse = new Course(10);
+        firstCourse = new Course(10);
 
         Lecture firstLecture = new Lecture (1, firstCourse.idCourse1);
         Lecture secondLecture = new Lecture(2, 2);
@@ -34,13 +37,49 @@ public class Main {
 
         //7
 
-        Scanner scanner = new Scanner(System.in);
-        int category;//для выбора категории меню
-        int selected;//для выбора создания лекции в первый раз
-        int idLectures;//для считывания ID лекции и пеередачи ее в параметр объекта лекции
-        int createLecture;//для выбора создания лекции во второй раз
-        int exit;//для возврата в меню или завершения
+        //Scanner scanner = new Scanner(System.in);
+       int exit;//для возврата в меню или завершения
+        //menu(); - создает меню для выбора категорий
+        //choiceOfCategory(); - обрабатывает выбор категорий
+        //dialog1(); - первый диалог про создание лекции
+        //severalLectures(); - создает несколько лекций
+        //orderLecture(); - запрашиваем и выводим данные лекции
+        //dialog2(); - диалог создать еще лекцию
+        //dialogExit(); - после создания лекций вернуться в меню или завершить программу
 
+menu();
+choiceOfCategory();
+
+dialog1();
+severalLectures();
+
+//После создания лекций или вернуться в меню или завершить программу.
+dialogExit();
+
+//Пока считанный результат 0 ИЛИ больше 1 прервать работу программы
+exit = scanner.nextInt();
+while (exit == 0 || exit > 1 ) {break;}
+if (exit == 1) {
+    menu();
+    choiceOfCategory();
+    dialog1();
+    severalLectures();
+    dialogExit();
+}
+exit = scanner.nextInt();
+while (exit == 0 || exit > 1 ) {break;}
+//scanner.close();
+
+    }
+    //------ область видимости за пределами класса мейн
+    static int category;//для выбора категории мен.
+                        // Объявляем за пределами метода main чтобы создать и воспользоваться методами
+    static int selected;//для выбора создания лекции в первый раз
+    static int idLectures;//для считывания ID лекции и пеередачи ее в параметр объекта лекции
+    static int createLecture;//для выбора создания лекции во второй раз
+    //7
+    static Scanner scanner = new Scanner(System.in);
+    public static void menu() {
         System.out.println("Выберите категорию");
         System.out.println("1 Курсы");
         System.out.println("2 Учителя");
@@ -48,9 +87,16 @@ public class Main {
         System.out.println("4 Лекции");
         System.out.println("0 Выйти");
         category = scanner.nextInt();
+    }
 
+    public static void choiceOfCategory(){
         switch (category)
         {
+            case 0:
+                System.out.println("Программа завершилась");
+                System.exit(0);//завершить программу
+                break;
+
             case 1:
                 System.out.println("Вы выбрали Курсы");
                 System.out.println("5 Вернуться в меню");
@@ -67,10 +113,6 @@ public class Main {
             case 4:
                 System.out.println("Вы выбрали Лекции");
                 break;
-            case 0:
-                    System.out.println("Программа завершилась");
-                    System.exit(0);//завершить программу
-
 
             default:
                 // Эта строка исправила ошибку. Если передавался ноль, то всё работало.
@@ -78,8 +120,7 @@ public class Main {
                 // Пока выбор равен нулю или больше 4 все работат правильно
                 while (category > 4) {
                     System.out.println("Пожалуйста выберите из предложенных цифр");
-                    category = Menu();
-
+                    menu();
                 }
 
                 while (category == 0) {
@@ -107,96 +148,60 @@ public class Main {
                     break;
                 }
         }
+    }
 
+    public static void dialog1(){
         System.out.println("Создать лекцию?");
         System.out.println("0 Нет");
         System.out.println("1 Да");
         selected = scanner.nextInt();
+    }
+    //7
+    public static void severalLectures(){
 
-
-        //selected = dialog(scanner);
         switch (selected)
         {
             case 0:
                 System.out.println("Вы выбрали не создавать лекцию");
                 break;
             case 1:
-                    System.out.println("Вы выбрали создать лекцию");
-                    System.out.println("Введите ID лекции");
-                    idLectures = scanner.nextInt();
-                    //System.out.println("Введите ID курса");
-                    //беру название переменной из класса Лекция конструктора для лекции
-                    //int courseId = scanner.nextInt();
-                    //Lecture nLecture = new Lecture(idLectures,courseId);
-                    //беру название переменной из класса Курс конструктора для лекции
-                    //int idCourse1 = scanner.nextInt();
-                    Lecture nLecture = new Lecture(idLectures,firstCourse.idCourse1);
-                    System.out.println(nLecture);
-                    System.out.println(firstCourse.idCourse1);
-                    System.out.println(Lecture.counterLectures);
+                System.out.println("Вы выбрали создать лекцию");
+                //беру название переменной из класса Лекция конструктора для лекции
+                orderLecture();
+                dialog2();
+                // В повторном диалоге использую 2 чтобы отличать
+                // Пока выбор = 2 создаем новую лекцию
+                while (createLecture == 2) {
+                    orderLecture();
+                    dialog2();
 
-                    System.out.println("Создать еще лекцию");
-                    System.out.println("0 Нет");
-                    System.out.println("2 Да");
-                dialog2(); // 8 Вынес диалог для создания повторной лекции в метод
-                     createLecture = scanner.nextInt();
-
-                    // 8. В 7 задаче было ограниченное количество возможностей для создания лекции
-                    // было через if. Изменил через цикл чтобы можно былло достигнуть какого-то числа
-                    // созданных лекций.
-                    while (createLecture == 2) {
-                        System.out.println("Введите ID лекции");
-                        idLectures = scanner.nextInt();
-                        //System.out.println("Введите ID курса");
-                        //беру название переменной из класса Курс конструктора для лекции
-                        //idCourse1 = scanner.nextInt();
-                        Lecture n1Lecture = new Lecture(idLectures,firstCourse.idCourse1);
-                        System.out.println(n1Lecture);
-                        System.out.println(firstCourse.idCourse1);
-                        System.out.println(Lecture.counterLectures);
-                        dialog2();
-                        // цикл работает с этой переменной. Если она не равна 2, то программа прерывается
-                        createLecture = scanner.nextInt();
-                        //8 Добавляю лимит на создание лекции и автоматическое завершение программы.
-                        // Счетчик лекций объявлен в классе Лекция. Если он достигает 9, то завершить программу
-                        if (Lecture.counterLectures == 9) {
-                            System.out.println("Вы достигли лимита по созданию лекций. Программа завершилась");
-                            //System.exit(0);//завершить программу
-                            } break;
-                    }
+                    //8 Добавляю лимит на создание лекции и автоматическое завершение программы.
+                    // Счетчик лекций объявлен в классе Лекция. Если он достигает 9, то завершить программу
+                    if (Lecture.counterLectures == 9) {
+                        System.out.println("Вы достигли лимита по созданию лекций.");
+                        //System.exit(0);//завершить программу
+                    } break;
+                }
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + selected);
 
         }
-        //После создания лекций или вернуться в меню или завершить программу.
-        dialogExit();
-        exit = scanner.nextInt();
-        while (exit == 0 || exit > 1 ) {break;}
-        if (exit == 1) {
-            Menu();
-        }
-scanner.close();
-
     }
-    //7
-    static Scanner scanner = new Scanner(System.in);
-    public static int Menu() {
-        System.out.println("Выберите категорию");
-        System.out.println("1 Курсы");
-        System.out.println("2 Учителя");
-        System.out.println("3 Студенты");
-        System.out.println("4 Лекции");
-        System.out.println("0 Выйти");
-        return scanner.nextInt();
+    public static void orderLecture(){
+    System.out.println("Введите ID лекции");
+    idLectures = scanner.nextInt();
+    Lecture nLecture = new Lecture(idLectures,firstCourse.idCourse1);
+    System.out.println(nLecture);
+    System.out.println(firstCourse.idCourse1);
+    System.out.println(Lecture.counterLectures);
     }
-
-    //7
 
     public static void dialog2() {
         System.out.println("Создать еще лекцию");
         System.out.println("0 Нет");
         System.out.println("2 Да");
+        createLecture = scanner.nextInt();
         }
 
     public static void dialogExit() {
